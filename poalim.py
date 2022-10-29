72410e9b64e56da985d64cc5d9251dcb9f1077f3
Jonathan Daniel <jonathand655@gmail.com>
2022-10-29 19:25:39 +0300

    Extract poalim exchange data with selenium
    Add selenium as a dependency.
    Install Chrome driver:
    https://chromedriver.chromium.org/downloads
    And add it to the PATH.
    Write a simple script to extract the data with
    selenium and pandas.
    
    There are 55 rows for 11 currencies since every
    currency has data from 5 days.
    
    Add the formatted HTML table to the data folder.

--- poalim.py
+++ poalim.py
@@ -1,6 +1,18 @@
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 POALIM_URL = "https://www.bankhapoalim.co.il/he/foreign-currency/exchange-rates"
+TABLE_ID = "fer-table-content"
 
-df = pd.read_html(POALIM_URL)
-print(f"{type(df) = }", df, sep="\n")
+driver = webdriver.Chrome()
+driver.get(POALIM_URL)
+elem = driver.find_element(By.ID, TABLE_ID)
+table_html = elem.get_attribute("innerHTML")
+driver.close()
+
+dfs = pd.read_html(table_html, header=0, skiprows=1, index_col=0)
+assert len(dfs) == 1
+df = dfs[0]
+print(df)
