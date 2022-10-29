8be7c1704de1e69e35414a98935c29caf9ec5cae
Jonathan Daniel <jonathand655@gmail.com>
2022-10-29 23:58:27 +0300

    Keep only the relevant columns
    Print the DataFrame neatly to the console.
    
    Still need to translate "KOD_MATBEA" into
    standard currency codes (USD, for example).

--- poalim.py
+++ poalim.py
@@ -8,6 +8,18 @@ _POALIM_GET_URL = "https://www.bankhapoalim.co.il/he/coin-rates"
 _DATE_QUERY = "?date="
 _POALIM_QUERY = _POALIM_GET_URL + _DATE_QUERY
 _DATE_FORMAT = "%Y-%m-%d"  # YYYY-MM-DD
+# _IRRELEVANT_COLS = ["CHANGE", "DATE_CHALIFIN", "DT_VALID", "TAX_ORDER"]
+_RELEVANT_COLS = [
+    "KOD_MATBEA",
+    "DT_ERECH",
+    "SHAAR_YATZIG",
+    "SHAAR_CHK_KNIA",
+    "SHAAR_CHK_MECHIRA",
+    "SHAAR_CASH_KNIA",
+    "SHAAR_CASH_MECHIRA",
+    "AHUZ_SHINUI",
+    "NAME_HEB",
+]
 
 
 def _get_url(t: datetime) -> str:
@@ -21,10 +33,15 @@ def get_df(t: datetime | None = None) -> pd.DataFrame:
         #  import calendar, calendar.SATURDAY or calendar.SUNDAY
         t = datetime.now(IL_TZ)
 
-    return pd.read_json(_get_url(t))
+    df = pd.read_json(_get_url(t))
+    df = df[_RELEVANT_COLS]
+    return df
 
 
 if __name__ == "__main__":
     df = get_df(datetime(2022, 10, 25))
-    print(df)
+    with pd.option_context(
+        'display.max_rows', None, 'display.max_columns', None, 'display.width', None
+    ):
+        print(df)
     print(df.iloc[0])
