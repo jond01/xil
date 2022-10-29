9388d12c13569010c206d1c883c0dbd68b57904c
Jonathan Daniel <jonathand655@gmail.com>
2022-10-29 23:42:01 +0300

    Read the data directly through pandas
    Remove urllib usage and import.

--- poalim.py
+++ poalim.py
@@ -1,9 +1,7 @@
-import urllib.request
-from zoneinfo import ZoneInfo
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import pandas as pd
-
 
 IL_TZ = ZoneInfo("Israel")
 _POALIM_GET_URL = "https://www.bankhapoalim.co.il/he/coin-rates"
@@ -16,19 +14,14 @@ def _get_url(t: datetime) -> str:
     return _POALIM_QUERY + t.strftime(_DATE_FORMAT)
 
 
-def _get_data(t: datetime | None = None) -> str:
+def get_df(t: datetime | None = None) -> pd.DataFrame:
     if t is None:
         # TODO: on Sunday and Saturday there are no exchange rates, choose the last
         #  active day. To check the day use t.weekday() and compare to:
         #  import calendar, calendar.SATURDAY or calendar.SUNDAY
         t = datetime.now(IL_TZ)
 
-    data = urllib.request.urlopen(_get_url(t)).read().decode()
-    return data
-
-
-def get_df(t: datetime | None = None) -> pd.DataFrame:
-    return pd.read_json(_get_data(t))
+    return pd.read_json(_get_url(t))
 
 
 if __name__ == "__main__":
