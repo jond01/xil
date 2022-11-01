280a625027ae2cab7471a124a5cdb7506cf157c0
Jonathan Daniel <jonathand655@gmail.com>
2022-11-02 00:21:39 +0200

    Extract Leumi exchange data
    A user agent is required, otherwise the request
    is blocked.
    The used user agent is taken from:
    https://www.useragentstring.com
    Alternatively, could use the Python package:
    https://github.com/hellysmile/fake-useragent
    And take a user agent from there.

---
+++ leumi.py
@@ -0,0 +1,31 @@
+"""
+Leumi's official exchange web page:
+https://www.leumi.co.il/Lobby/currency_rates/40806/
+embeds the table from:
+https://www.bankleumi.co.il/vgnprod/currency/new_shaar_muskamim.html
+which in turn consumes its data from:
+https://www.bankleumi.co.il/vgnprod/currency/ajax/new_shaar_muskamim_data.json
+
+Historical data by currency ID:
+https://www.bankleumi.co.il/vgnprod/currency/ExchangeRateByCurrency.aspx?in_matbea=1
+
+For business data:
+https://biz.leumi.co.il/portal/site/Business/home_03/currency_rates/12283/
+the table is embedded from the static page:
+https://www.bankleumi.co.il/vgnprod/ltrade_new_shaar_muskamim_multilang_vgn_HE.html
+
+Looks like the business data is identical to the private data.
+"""
+import pandas as pd
+
+LEUMI_URL = "\
+https://www.bankleumi.co.il/vgnprod/currency/ajax/new_shaar_muskamim_data.json"
+USER_AGENT = "\
+Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) \
+Chrome/104.0.5112.79 Safari/537.36"
+
+s = pd.read_json(LEUMI_URL, typ="series", storage_options={'User-Agent': USER_AGENT})
+date = s.yatzigDate  # Hour in `s.topHeaderText`
+df = pd.DataFrame.from_records(s.data)
+
+print(date, df, sep="\n")
