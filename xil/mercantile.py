5e8e7639a2c873e998bc49d2ff33843ddf18a4db
Jonathan Daniel <jonathand655@gmail.com>
2022-11-19 23:29:32 +0200

    Add Mercantile bank data
    Mercantile's foreign exchange rates' structure is
    identical to Discount's, but the values are not the
    same.
    This bank is fully owned by Discount.
    TODO: instead of duplicating code, use shared
    modules and inheritance.

---
+++ xil/mercantile.py
@@ -0,0 +1,18 @@
+"""
+Mercantile bank
+
+https://www.mercantile.co.il/MB/private/foregin-currency/exchange-rate
+
+The structure is identical Discount's, but the data is different.
+"""
+# TODO: avoid code duplication between this module and `discount`.
+import pandas as pd
+
+_MERCANTILE_URL = "https://www.mercantile.co.il/MB/private/foregin-currency/exchange-rate"
+
+df = pd.read_html(_MERCANTILE_URL, header=[0, 1])[0]
+
+idx0 = pd.MultiIndex.from_product([["currency"], ["amount", "code", "official rate"]])
+idx1 = pd.MultiIndex.from_product([["transfer", "cash"], ["buy", "sell"]])
+idx = idx0.append(idx1)
+df.columns = idx
