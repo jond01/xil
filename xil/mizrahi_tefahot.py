883cce7377b905160f21bc1917f269df3a57e90a
Jonathan Daniel <jonathand655@gmail.com>
2022-11-27 20:20:40 +0200

    Normalize Mizrahi data
    Use `.apply(...)` and not `.map(...)` on the columns
    since the `map` method is not recognized by mypy.
    
    Consider using custom stubs and not only
    `pandas-stubs` if problems continue.
    https://mypy.readthedocs.io/en/stable/stubs.html

--- xil/mizrahi_tefahot.py
+++ xil/mizrahi_tefahot.py
@@ -6,6 +6,23 @@ https://www.mizrahi-tefahot.co.il/brokerage/currancyexchange/
 """
 import pandas as pd
 
-_MIZRAHI_TEFAHOT_URL = "https://www.mizrahi-tefahot.co.il/brokerage/foreignexchange/"
+from xil._currencies import currency_from_heb_name
 
-df = pd.read_html(_MIZRAHI_TEFAHOT_URL, header=0)[0]
+_MIZRAHI_TEFAHOT_URL = "https://www.mizrahi-tefahot.co.il/brokerage/foreignexchange/"
+_IDX0 = pd.MultiIndex.from_product([["currency"], ["name", "amount", "official rate"]])
+_IDX1 = pd.MultiIndex.from_product([["cash", "transfer"], ["buy", "sell"]])
+_MIZRAHI_TEFAHOT_IDX = _IDX0.append(_IDX1)
+
+
+def get_mizrahi_teafhot_df(url: str = _MIZRAHI_TEFAHOT_URL) -> pd.DataFrame:
+    """Get Mizrahi Tefahot Bank exchange rates"""
+    df = pd.read_html(url, header=0)[0]
+    df.columns = _MIZRAHI_TEFAHOT_IDX
+    df[("currency", "code")] = df[("currency", "name")].apply(currency_from_heb_name)
+    df = df.drop(labels=("currency", "name"), axis=1)
+    df = df.set_index(("currency", "code"))
+    return df
+
+
+if __name__ == "__main__":
+    print(get_mizrahi_teafhot_df())
