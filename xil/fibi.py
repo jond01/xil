362d1580b2cb7d385c9b60f5394f021b661177fc
Jonathan Daniel <jonathand655@gmail.com>
2022-12-06 22:51:29 +0200

    Normalize FIBI's data (script)
    It's still a script, and the columns are different than
    other banks.

--- xil/fibi.py
+++ xil/fibi.py
@@ -8,6 +8,7 @@ https://apps.fibi.co.il/Matach/matach.aspx
 """
 import pandas as pd
 
+from xil._currencies import currency_from_heb_name
 from xil._headers import get_url_response
 
 _FIBI_URL = "http://apps.fibi.co.il/Matach/matach.aspx"
@@ -23,6 +24,9 @@ _RELEVANT_COLS = [
     ("יחידה", "יחידה"),
     ("מטבע", "מטבע"),
 ]
+_IDX0 = pd.MultiIndex.from_product([["Spot", "Same day"], ["low", "high"]])
+_IDX1 = pd.MultiIndex.from_product([["currency"], ["amount", "name"]])
+_IDX = _IDX0.append(_IDX1)
 
 with get_url_response(_FIBI_URL) as response:
     dfs = pd.read_html(
@@ -35,3 +39,7 @@ with get_url_response(_FIBI_URL) as response:
 
 df = dfs[0]  # It is guaranteed to have at least one element - otherwise an exception
 df = df[_RELEVANT_COLS]
+df.columns = _IDX
+df[("currency", "code")] = df[("currency", "name")].apply(currency_from_heb_name)
+df = df.drop(labels=("currency", "name"), axis=1)
+df = df.set_index(("currency", "code"))
