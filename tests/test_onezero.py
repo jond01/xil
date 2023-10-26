da6cb05272638ecb93c799057a6baa65fa8871a9
Jonathan Daniel <jonathand655@gmail.com>
2023-10-26 22:21:52 +0300

    Add onezero live test

---
+++ tests/test_onezero.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+import pandas as pd
+import pytest
+
+from xil._currencies import CurrencyCode
+from xil.onezero import get_onezero_df
+
+
+@pytest.fixture(name="df")
+def df_fixture() -> pd.DataFrame:
+    return get_onezero_df()
+
+
+def test_df(df: pd.DataFrame) -> None:
+    assert (
+        df.index == [CurrencyCode.EUR, CurrencyCode.USD]
+    ).all(), "The currencies are not as expected"
+    assert (
+        df[("transfer", "buy")] < df[("currency", "official rate")]
+    ).all(), "The buy rate is not lower than the official rate"
+    assert (
+        df[("currency", "official rate")] < df[("transfer", "sell")]
+    ).all(), "The sell rate is not higher than the official rate"
