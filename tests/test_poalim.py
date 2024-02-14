d0fb832de109d36c56d8a285a32cb911751b8311
Jonathan Daniel <jonathand655@gmail.com>
2024-02-14 19:34:11 +0200

    Test empty Poalim data

--- tests/test_poalim.py
+++ tests/test_poalim.py
@@ -1,10 +1,11 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring
-from datetime import date
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 import calendar
+from datetime import date
 
+import pandas as pd
 import pytest
 
-from xil.poalim import IL_TZ, _get_url
+from xil.poalim import _get_url, _il_date, get_df
 
 
 def _is_weekend(t: date) -> bool:
@@ -28,3 +29,30 @@ def _is_weekend(t: date) -> bool:
 def test_get_url(t: date, expected_url: str) -> None:
     assert _get_url(t) == expected_url
 
+
+@pytest.mark.live
+class TestEmptyData:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "t",
+        [
+            pytest.param(date(2024, 2, 3), id="Saturday 2023-02-03"),
+            pytest.param(date(2024, 2, 4), id="Sunday 2023-02-04"),
+        ],
+    )
+    def test_weekend(t: date) -> None:
+        """Weekend in this context is Saturday and Sunday"""
+        assert pd.read_json(  # pylint: disable=no-member
+            _get_url(t)
+        ).empty, "The df is nonempty"
+
+    @staticmethod
+    @pytest.fixture()
+    def empty_today() -> bool:
+        return _is_weekend(_il_date())
+
+    @staticmethod
+    def test_today(empty_today: bool) -> None:
+        assert (
+            get_df().empty == empty_today
+        ), "The df emptiness is different than expected"
