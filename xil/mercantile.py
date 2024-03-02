247917d4929547aa3cd5c8c794445ac0e0d308a9
Jonathan Daniel <jonathand655@gmail.com>
2024-03-02 20:00:13 +0200

    Update Mercantile links

--- xil/mercantile.py
+++ xil/mercantile.py
@@ -1,7 +1,9 @@
 """
 Mercantile bank
 
-https://www.mercantile.co.il/MB/private/foregin-currency/exchange-rate
+https://www.mercantile.co.il/en/private/foregin-currency/exchange-rates/
+Taken from:
+https://www.mercantile.co.il/api/ExchangeRates/GetExchangeRates?bankCode=0017
 
 The structure is identical Discount's, but the data is different.
 """
@@ -10,7 +12,7 @@ import pandas as pd
 from xil.discount import get_discount_df
 
 _MERCANTILE_URL = "\
-https://www.mercantile.co.il/MB/private/foregin-currency/exchange-rate"
+https://www.mercantile.co.il/api/ExchangeRates/GetExchangeRates?bankCode=0017"
 
 
 def get_mercantile_df(url: str = _MERCANTILE_URL) -> pd.DataFrame:
