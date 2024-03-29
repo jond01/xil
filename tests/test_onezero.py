db045265b410eef3b09b3691bf5679485424020d
Jonathan Daniel <jonathand655@gmail.com>
2024-03-29 12:59:12 +0300

    Remove the comparisons to the official rate in 01
    Fixes #146

--- tests/test_onezero.py
+++ tests/test_onezero.py
@@ -17,8 +17,5 @@ def test_df(df: pd.DataFrame) -> None:
         df.index == [CurrencyCode.EUR, CurrencyCode.USD]
     ).all(), "The currencies are not as expected"
     assert (
-        df[("transfer", "buy")] < df[("currency", "official rate")]
-    ).all(), "The buy rate is not lower than the official rate"
-    assert (
-        df[("currency", "official rate")] < df[("transfer", "sell")]
-    ).all(), "The sell rate is not higher than the official rate"
+        df[("transfer", "buy")] < df[("transfer", "sell")]
+    ).all(), "The buy rate is not lower than the sell rate"
