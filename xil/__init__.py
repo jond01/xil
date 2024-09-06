cb70bddfad0e5328d77faad90a82e8fd0611d283
Jonathan Daniel <jonathand655@gmail.com>
2024-09-06 16:30:15 +0300

    Satisfy Ruff - should probably report it
    Not using F401 noqa conflicts with PLC0414
    See:
    https://github.com/astral-sh/ruff/issues/6294#issuecomment-2153751697

--- xil/__init__.py
+++ xil/__init__.py
@@ -1,3 +1,4 @@
 """The XIL package for fetching Israeli banks' ILS exchange rates"""
+# pylint: disable=useless-import-alias
 
-from xil._version import __version__
+from xil._version import __version__ as __version__  # noqa: PLC0414
