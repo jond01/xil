52780486f27a2fefd0a1eb70f256514cbda6295e
Jonathan Daniel <jonathand655@gmail.com>
2024-09-06 14:47:41 +0300

    Add dynamic project version
    https://backend.pdm-project.org/metadata/#dynamic-project-version

---
+++ pdm_version.py
@@ -0,0 +1,15 @@
+import os
+
+from pdm.backend.hooks.version import SCMVersion
+
+
+def format_version(version: SCMVersion) -> str:
+    if version.distance is None and not version.dirty:
+        # Official version
+        return str(version.version)
+    elif os.getenv("PYPI_PUBLISH"):
+        # Published version
+        return f"{version.version}.dev{version.distance or ''}"
+    else:
+        # Local version
+        return f"{version.version}.dev{version.distance or ''}+{version.node}"
