c6e615cf6f9eb75bb3b510c0ece229d66eac467a
Jonathan Daniel <jonathand655@gmail.com>
2024-09-06 16:44:02 +0300

    Add noqa F401 - `__init__.py` is not a stub
    And it is not desired to define it in `__all__`

--- xil/__init__.py
+++ xil/__init__.py
@@ -1,4 +1,3 @@
 """The XIL package for fetching Israeli banks' ILS exchange rates"""
-# pylint: disable=useless-import-alias
 
-from xil._version import __version__ as __version__  # noqa: PLC0414
+from xil._version import __version__  # noqa: F401
